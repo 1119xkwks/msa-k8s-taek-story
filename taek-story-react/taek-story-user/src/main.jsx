b4dc6f6ea8b7d78492a6b332f268447712e7f846
yt.kwon <ytkwon91@gmail.com>
2025-08-08 13:25:21 +0900

    redux 설치 및 사용

--- taek-story-react/taek-story-user/src/main.jsx
+++ taek-story-react/taek-story-user/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './App.css' // Tailwind CSS를 위해 App.css import
-import './assets/fonts/notosanskr/fonts.css' // NotoSansKR 폰트 import
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom";
-import { Provider } from 'react-redux'
-import store from './store/index.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./App.css"; // Tailwind CSS를 위해 App.css import
+import "./assets/fonts/notosanskr/fonts.css"; // NotoSansKR 폰트 import
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/index.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -16,4 +16,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </Provider>
   </StrictMode>,
-)
+);
