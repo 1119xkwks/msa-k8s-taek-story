5ba5a70a81a63012d8bfa6734eb2fa0b23c9b63d
yt.kwon <ytkwon91@gmail.com>
2025-08-08 13:27:38 +0900

    notification

--- taek-story-react/taek-story-user/src/components/drawer/RightMenuDrawer.jsx
+++ taek-story-react/taek-story-user/src/components/drawer/RightMenuDrawer.jsx
@@ -6,7 +6,7 @@ import {
   faMagnifyingGlass,
   faArrowRightFromBracket,
   faHome,
-  faUserGear,
+  faBell,
 } from "@fortawesome/free-solid-svg-icons";
 
 import {
@@ -22,76 +22,74 @@ import {
 
 const RightMenuDrawer = ({ isMenuOpen, menuCloseHandler }) => {
   return (
-    <>
-      <Drawer
-        className="right-side-menu"
-        open={isMenuOpen}
-        onClose={menuCloseHandler}
-        position="right"
-      >
-        <DrawerHeader title="MENU" titleIcon={() => <></>} />
-        <DrawerItems>
-          <Sidebar
-            aria-label="Sidebar with multi-level dropdown example"
-            className="[&>div]:bg-transparent [&>div]:p-0"
-          >
-            <div className="flex h-full flex-col justify-between py-2">
-              <div>
-                <form className="pb-3 md:hidden">
-                  <TextInput
-                    type="search"
-                    placeholder="탐색"
-                    required
-                    size={32}
-                    icon={() => <FontAwesomeIcon icon={faMagnifyingGlass} />}
-                  />
-                </form>
-                <SidebarItems>
-                  <SidebarItemGroup>
-                    <SidebarItem
-                      href="/"
-                      icon={() => <FontAwesomeIcon icon={faHome} />}
-                    >
-                      홈
-                    </SidebarItem>
-                    <SidebarItem
-                      href="/#"
-                      icon={() => <FontAwesomeIcon icon={faUserGear} />}
-                    >
-                      계정
-                    </SidebarItem>
-                  </SidebarItemGroup>
-                  <SidebarItemGroup>
-                    <SidebarItem
-                      href="/login"
-                      icon={() => (
-                        <FontAwesomeIcon icon={faArrowRightToBracket} />
-                      )}
-                    >
-                      로그인
-                    </SidebarItem>
-                    <SidebarItem
-                      href="/signup"
-                      icon={() => <FontAwesomeIcon icon={faUserPlus} />}
-                    >
-                      회원가입
-                    </SidebarItem>
-                    <SidebarItem
-                      href="#"
-                      icon={() => (
-                        <FontAwesomeIcon icon={faArrowRightFromBracket} />
-                      )}
-                    >
-                      로그아웃
-                    </SidebarItem>
-                  </SidebarItemGroup>
-                </SidebarItems>
-              </div>
+    <Drawer
+      className="right-side-menu"
+      open={isMenuOpen}
+      onClose={menuCloseHandler}
+      position="right"
+    >
+      <DrawerHeader title="MENU" titleIcon={() => <></>} />
+      <DrawerItems>
+        <Sidebar
+          aria-label="Sidebar with multi-level dropdown example"
+          className="[&>div]:bg-transparent [&>div]:p-0"
+        >
+          <div className="flex h-full flex-col justify-between py-2">
+            <div>
+              <form className="pb-3 md:hidden">
+                <TextInput
+                  type="search"
+                  placeholder="탐색"
+                  required
+                  size={32}
+                  icon={() => <FontAwesomeIcon icon={faMagnifyingGlass} />}
+                />
+              </form>
+              <SidebarItems>
+                <SidebarItemGroup>
+                  <SidebarItem
+                    href="/"
+                    icon={() => <FontAwesomeIcon icon={faHome} />}
+                  >
+                    홈
+                  </SidebarItem>
+                  <SidebarItem
+                    href="/#"
+                    icon={() => <FontAwesomeIcon icon={faBell} />}
+                  >
+                    Notification
+                  </SidebarItem>
+                </SidebarItemGroup>
+                <SidebarItemGroup>
+                  <SidebarItem
+                    href="/login"
+                    icon={() => (
+                      <FontAwesomeIcon icon={faArrowRightToBracket} />
+                    )}
+                  >
+                    로그인
+                  </SidebarItem>
+                  <SidebarItem
+                    href="/signup"
+                    icon={() => <FontAwesomeIcon icon={faUserPlus} />}
+                  >
+                    회원가입
+                  </SidebarItem>
+                  <SidebarItem
+                    href="#"
+                    icon={() => (
+                      <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                    )}
+                  >
+                    로그아웃
+                  </SidebarItem>
+                </SidebarItemGroup>
+              </SidebarItems>
             </div>
-          </Sidebar>
-        </DrawerItems>
-      </Drawer>
-    </>
+          </div>
+        </Sidebar>
+      </DrawerItems>
+    </Drawer>
   );
 };
 
