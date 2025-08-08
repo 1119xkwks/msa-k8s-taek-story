eb087ebdf37c1b3732354150ba9c0602c5993de8
yt.kwon <ytkwon91@gmail.com>
2025-08-08 11:01:57 +0900

    댓글 와꾸

--- taek-story-react/taek-story-user/src/components/card/PostCard.jsx
+++ taek-story-react/taek-story-user/src/components/card/PostCard.jsx
@@ -4,13 +4,14 @@ import PostCardHeader from "/src/components/post/PostCardHeader.jsx";
 import PostCardContent from "/src/components/post/PostCardContent.jsx";
 import PostCardComment from "/src/components/post/PostCardComment.jsx";
 import PostCardFooter from "/src/components/post/PostCardFooter.jsx";
+import MyDivider from "../layout/MyDivider.jsx";
 
 const PostCard = () => {
   return (
     <MyCard className="post-card">
       <PostCardHeader />
       <PostCardContent />
-      <hr className="my-1 border-t border-gray-300" />
+      <MyDivider className="my-1" />
       <PostCardComment />
       <PostCardFooter />
     </MyCard>
---
+++ taek-story-react/taek-story-user/src/components/layout/MyDivider.jsx
@@ -0,0 +1,7 @@
+import "./MyDivider.css";
+
+const MyDivider = ({ className }) => {
+  return <hr className={`my-divider ${className}`} />;
+};
+
+export default MyDivider;
